428205b91f618bc0dd002001aaf6873fa0d51dec
OptimisticUnderdog <mabunda.wealth@gmail.com>
2022-12-01 05:03:04 -0800

    hot fix

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -2,11 +2,7 @@
 
 if __name__ == "__main__":
     import sys
-    argv = sys.argv[1:]
-    argv_count = len(argv)
-    index = 1
-    res = 0
-    while index <= argv_count:
-        res += int(sys.argv[index])
-        index += 1
-        print("{:d}".format(res))
+    total = 0
+    for i in range(len(sys.argv) - 1):
+        total += int(sys.argv[i + 1])
+         print("{}".format(total))
