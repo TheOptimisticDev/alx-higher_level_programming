e09cc372469cbf3a07531e1fd9a1ad31fdc2e989
OptimisticUnderdog <mabunda.wealth@gmail.com>
2023-01-31 00:54:11 -0800

    commit

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 
 def read_file(filename=""):
-    with open(file_path, "r", encoding="utf-8") as file:
-        content = file.read()
-        print(content)
+    with open(filename, "r", encoding="UTF-8") as file:
+        print(f.read(), end="")
