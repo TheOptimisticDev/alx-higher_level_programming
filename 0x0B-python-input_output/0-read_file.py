4eec1910d5f788e435471cd95c4c3d01bf42b061
OptimisticUnderdog <mabunda.wealth@gmail.com>
2023-02-28 12:25:58 -0800

    commit

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,5 +1,17 @@
 #!/usr/bin/python3
+""" This module that contains a function which reads from a file """
+
 
 def read_file(filename=""):
-    with open(filename, "r", encoding="UTF-8") as file:
-        print(f.read(), end="")
+    """
+    Function that reads from a file
+    Args:
+        filename: filename
+    Raises:
+        Exception: when the file can be opened
+    """
+
+    with open(filename, "r", encoding="utf-8") as f:
+        read_data = f.read()
+        print(read_data, end="")
+
