ac50906dfc525e64ebd3f9265c7adb3ff4071f40
OptimisticUnderdog <mabunda.wealth@gmail.com>
2023-03-06 03:43:01 -0800

    commit

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,7 +1,10 @@
--- creates the database hbtn_0d_2 and the user user_0d_2.
--- user_0d_2 should have only SELECT privilege in the database hbtn_0d_2
-CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
-CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost';
-SET PASSWORD FOR 'user_0d_2'@'localhost' = PASSWORD('user_0d_2_pwd');
-GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
-FLUSH PRIVILEGES;
+-- Create the database `hbtn_0d_2` and the user `user_0d_2`.
+-- The `user_0d_2` has SELECT privilege on hbtn_0d_2 with password `user_0d_2_pwd`
+CREATE DATABASE
+    IF NOT EXISTS `hbtn_0d_2`;
+CREATE USER
+    IF NOT EXISTS 'user_0d_2'@'localhost'
+    IDENTIFIED BY 'user_0d_2_pwd';
+GRANT SELECT
+   ON `hbtn_0d_2`.*
+   TO 'user_0d_2'@'localhost';
