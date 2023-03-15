f0e53de08c7a9081c98ee03aff4dfcb7c4af6914
OptimisticUnderdog <mabunda.wealth@gmail.com>
2023-03-15 01:02:37 -0700

    commit

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -2,8 +2,7 @@
 // prints 3 lines: (like 1-multi_languages.js) but by
 // using an array of string and a loop
 
-const langs = ['C is fun', 'Python is cool', 'Javascript is amazing'];
-
-for (let i = 0; i < 3; i++) {
-  console.log(langs[i]);
+const strings = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+for (let i = 0; i < strings.length; i++) {
+  console.log(strings[i]);
 }
