5095f4fa2b57f6c25c3447657d652106a48c4966
OptimisticUnderdog <mabunda.wealth@gmail.com>
2023-06-20 10:09:51 -0700

    update

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
-def no_c(my_string):
-    "removes all characters c and C from a string"
 
-    "new string to return"
-    no_c_string = ""
-    for i in range(len(my_string)):
-        "append if the char is not 'c' or 'C'"
-        if my_string[i] != 'c' and my_string[i] != 'C':
-            no_c_string += my_string[i]
-        return (no_c_string)
+def no_c(my_string):
+
+    new_string = ""
+
+    for i in my_string:
+        if i == 'c' or i == "C":
+            pass
+        else:
+            new_string += i
+    return new_string
--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,14 +1,8 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    "prints a matrix of integers"
-
-    row_len = len(matrix)
-    col_len = len(matrix[0])
-
-    for i in range(row_len):
-        for j in range(col_len):
-            if j != col_len - 1:
-                print("{:d}".format(matrix[i][j]), end=' ')
-            else:
-                print("{:d}".format(matrix[i][j]), end='')
-            print("")
+    for i in range(len(matrix)):
+        for j in range(len(matrix[i])):
+            if j != 0:
+                print(" ", end='')
+            print("{:d}".format(matrix[i][j]), end='')
+        print()
