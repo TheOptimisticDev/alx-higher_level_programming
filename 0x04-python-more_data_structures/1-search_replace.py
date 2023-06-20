31e1bcba4f9f0a8a28a3abb7707b22ab9ded411d
OptimisticUnderdog <mabunda.wealth@gmail.com>
2023-06-20 10:34:18 -0700

    update

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,4 +1,10 @@
 #!/usr/bin/python3
-def search_replace(my_list, search, replace):
-    return [find if find != search else replace for find in my_list]
 
+def search_replace(my_list, search, replace):
+    new_list = []
+    for i in my_list:
+        if search == i:
+            new_list.append(replace)
+        else:
+            new_list.append(i)
+    return (new_list)
